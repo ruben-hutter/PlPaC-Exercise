4a94969ab0a5de83555c8d1c1e2681745f9ea164
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-04-17 11:27:15 +0200

    added exercise 5. code in question_5 folder, answer to c) is in the answers file

--- cs109-e1/question_5/main.cpp
+++ cs109-e1/question_5/main.cpp
@@ -13,25 +13,38 @@ enum Canids {
 	Canis_Simensis
 };
 
-enum Canids_Common_Name {
-	Golden_jackal,
-	Dog,
-	Coyote,
-	African_wolf,
-	Wolf,
-	Red_wolf,
-	Ethiopian_wolf
-} commonName;
-
 // takes a Canids and prints his common name
 void canidsCommonName(Canids c) {
-	Canids_Common_Name cmn_name;
-	cout << cmn_name << endl;
+	string commonName;
+	switch(c) {
+		case 0:
+			commonName = "Golden Jackal";
+			break;
+		case 1: // you can also write Canis_Familiaris
+			commonName = "Dog";
+			break;
+		case 2:
+			commonName = "Coyote";
+			break;
+		case 3:
+			commonName = "African Wolf";
+			break;
+		case 4:
+			commonName = "Wolf";
+			break;
+		case 5:
+			commonName = "Red Wolf";
+			break;
+		case 6:
+			commonName = "Ethiopian Wolf";
+			break;
+	}
+	cout << commonName << endl;
 }
 
 int main() {
 	Canids c = Canis_Familiaris;
-	//canidsCommonName(c);
+	canidsCommonName(c);
 	return 0;
 }
 
