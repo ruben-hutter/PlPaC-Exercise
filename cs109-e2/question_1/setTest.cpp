9c0945942b135aa62e3017fbb145ac56d1aca6bf
Tobias Hafner <t.hafner@stud.unibas.ch>
2022-04-25 19:09:35 +0200

    add debug prints to ex1

--- cs109-e2/question_1/setTest.cpp
+++ cs109-e2/question_1/setTest.cpp
@@ -5,22 +5,28 @@ using namespace std;
 
 int main ()
 {
+    cout << "creating set";
 	Set testset;
     
+    cout << "pop'ing set";
     testset.add(1.1, 2.2, "test0", 0);
     testset.add(2.2, 4.4, "test1", 1);
     testset.add(3.3, 6.6, "test2", 2);
 
+    cout << "rint n sort";
     testset.print();
 
     testset.sort();
 
+    cout << "test contains";
     cout << testset.contains(1);
     cout << testset.contains(5);
 
+    cout << "test remove";
     testset.remove(1);
 
     testset.print();
 
+    cout << "test size";
     cout << testset.size();
 }
