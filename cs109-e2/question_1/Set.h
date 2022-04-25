3acea2008cae4a8d91765b3d548aaf1b96d6e217
Tobias Hafner <t.hafner@stud.unibas.ch>
2022-04-25 19:04:59 +0200

    git madness part 2

--- cs109-e2/question_1/Set.h
+++ cs109-e2/question_1/Set.h
@@ -1,1 +1,54 @@
 //scheisse
+#ifndef SET_H
+#define SET_H
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <cmath>
+
+using namespace std;
+
+class Set
+{
+public:
+    // struct to store entries
+	struct Entry
+	{
+		int uid;
+		float firstFloat;
+		float secondFloat;
+		string entryString;
+        bool operator <(const Entry &entry)
+        {
+            float entryNorm = sqrt(entry.firstFloat*entry.firstFloat + entry.secondFloat*entry.secondFloat);
+			float ownNorm = sqrt(firstFloat*firstFloat + secondFloat*secondFloat);
+			return entryNorm < ownNorm;
+        }
+	} entry;
+
+	// adds element to list
+	int add(double x, double y, string name, int id = -1);
+
+	// removes the element with the specified id
+	int remove(int id);
+
+	// returns 1 if found, 0 if not found
+	int contains(int id);
+
+	// return number of elements in list
+	unsigned int size();
+
+	// sort elems by norm
+	void sort(int sort_type = 0, bool reorganize = false);
+
+	// print all entries to sdt::cout
+	void print();
+
+	void destroy();
+
+private:
+	list<Entry> entryList;
+};
+
+#endif
