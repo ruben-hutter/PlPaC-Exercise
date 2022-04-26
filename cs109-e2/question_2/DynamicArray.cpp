9abd55e359f193a87afc53a9c324721cc06b1171
Tobias Hafner <t.hafner@stud.unibas.ch>
2022-04-26 11:33:53 +0200

    add function sleleton for dynArray

---
+++ cs109-e2/question_2/DynamicArray.cpp
@@ -0,0 +1,35 @@
+// dynamic array implementation
+#include "dynamicArray.h"
+
+DynamicArray::DynamicArray(int initialSize) {
+
+}
+
+DynamicArray::~DynamicArray() {
+
+}
+
+// gets specific element
+int DynamicArray::get() {
+
+}
+
+// change element
+void DynamicArray::set(int position, int element) {
+
+}
+
+// add an element
+void DynamicArray::add(int element) {
+
+}
+
+// remove element
+void DynamicArray::remove() {
+
+}
+
+// executes the buffered changes at sets the available memory to 3/2
+void DynamicArray::trim(){
+
+}
