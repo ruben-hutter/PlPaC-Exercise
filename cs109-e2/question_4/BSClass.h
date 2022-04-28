0ceebbb20d491d99220ecad702d6463be4f58d9c
Tobias Hafner <t.hafner@stud.unibas.ch>
2022-04-28 04:53:36 +0200

    moved templ init to header

--- cs109-e2/question_4/BSClass.h
+++ cs109-e2/question_4/BSClass.h
@@ -9,17 +9,50 @@ using namespace std;
 
 template <typename T>
 class BSClass {
-    std::list<T> e_list;
+    std::list<T> e_list = std::list<T>;
 public:
-    void sort();
-    void add_elem(T elem);
-    void erase_elem(int index);
-    T get_elem(int index);
-    void add(int index, T operand);
-    void subtract(int index, T operand);
-    void mult(int index, T operand);
-    void div(int index, T operand);
-    void print();
+    void sort() {
+        e_list.sort();
+    };
+
+    void add_elem(T elem) {
+        e_list.push_back(elem);
+    };
+
+    void erase_elem(int index) {
+        e_list.erase(index);
+    };
+
+    T get_elem(int index) {
+        auto e_ptr = e_list.begin();
+        std::advance(e_ptr, index):
+        return *e_ptr;
+    };
+
+    void add(int index, T operand) {
+        get_elem(index) += operand;
+        sort();
+    };
+
+    void subtract(int index, T operand) {
+        get_elem(index) -= operand;
+        sort();
+    };
+
+    void mult(int index, T operand) {
+        get_elem(index) *= operand;
+        sort();
+    };
+
+    void div(int index, T operand) {
+        get_elem(index) /= operand;
+        sort();
+    };
+
+    void print() {
+        for (auto e : e_list)
+            std::cout << v << "\n";
+    };
 };
 
 #endif
