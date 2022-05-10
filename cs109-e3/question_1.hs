bddbdbc1b86bf440e1db3181ddfa164cfcabad5d
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-10 09:36:58 +0200

    1b correct solution, 1a still to modify

--- cs109-e3/question_1.hs
+++ cs109-e3/question_1.hs
@@ -1,7 +1,8 @@
 -- a) find ith element of a list
 myList = [1, 2, 3, 4]
-
 atIndex = myList !! 2
-atIndex1 = (!!) myList 2
 
 -- b) generate a list of tuples (n,s)
+tupleList = [ (x,y) | x <- [3,6..3*25], y <- [sum [1..x]]]
+
+-- c) remove all instances from a list
