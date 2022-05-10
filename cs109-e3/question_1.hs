40d4f7b4e768b2c1e4cb3ffd2d837eb4578c9687
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-10 23:34:35 +0200

    added implementation up to 1e

--- cs109-e3/question_1.hs
+++ cs109-e3/question_1.hs
@@ -1,10 +1,14 @@
 -- a) find ith element of a list
-atIndex :: Int -> [a] -> a
-atIndex n myList | n == 0    = head myList
-                 | otherwise = last (take (n+1) myList)
+atIndex n myList = last (take (n+1) myList)
 
 -- b) generate a list of tuples (n,s)
-tupleList = [ (x,y) | x <- [3,6..3*25], y <- [sum [1..x]]]
+tupleList = [(x,y) | x <- [3,6..3*25], y <- [sum [1..x]]]
 
 -- c) remove all instances from a list
+removeItem i myList = [x | x <- myList, x /= i]
 
+-- d) list containing divisors of integer
+getDivisors n = [x | x <- [1..], mod n x == 0]
+
+-- e) list containing shared prime factors of 2 integers
+getPrimes n m = [x | x <- [1..(max n m)], x ]
