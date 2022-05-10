7f26db099d21430aaadf62792cb04bc8e1f3544a
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-11 00:04:42 +0200

    up to f

--- cs109-e3/question_1.hs
+++ cs109-e3/question_1.hs
@@ -1,3 +1,4 @@
+import Data.Bits (Bits(xor))
 -- a) find ith element of a list
 atIndex n myList = last (take (n+1) myList)
 
@@ -8,7 +9,11 @@ tupleList = [(x,y) | x <- [3,6..3*25], y <- [sum [1..x]]]
 removeItem i myList = [x | x <- myList, x /= i]
 
 -- d) list containing divisors of integer
-getDivisors n = [x | x <- [1..], mod n x == 0]
+getDivisors n = [x | x <- [1..n], mod n x == 0]
 
 -- e) list containing shared prime factors of 2 integers
-getPrimes n m = [x | x <- [1..(max n m)], x ]
+isPrime k = (k > 1) && null [ x | x <- [2..k - 1], k `mod` x == 0]
+getPrimes n m = [x | x <- getDivisors n, x `elem` getDivisors m, isPrime x]
+
+-- f) xor two strings
+xorStrings s1 s2 = 0
