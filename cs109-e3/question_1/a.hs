635196b62a319f988815f834167a3d6d3c55f7ca
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-10 08:30:51 +0200

    changed file name and added b

--- cs109-e3/question_1/a.hs
+++
@@ -1,4 +0,0 @@
--- find ith element of a list
-myList = [1, 2, 3, 4]
-
-atIndex = myList !! 2
---
+++ cs109-e3/question_1/question_1.hs
@@ -0,0 +1,7 @@
+-- a) find ith element of a list
+myList = [1, 2, 3, 4]
+
+atIndex = myList !! 2
+atIndex1 = (!!) myList 2
+
+-- b) generate a list of tuples (n,s)
