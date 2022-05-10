453b0291b897e79a932cab300c10849fba1baa22
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-10 10:02:16 +0200

    modified 1a... maybe correct way, idk

--- cs109-e3/question_1.hs
+++ cs109-e3/question_1.hs
@@ -1,8 +1,10 @@
 -- a) find ith element of a list
-myList = [1, 2, 3, 4]
-atIndex = myList !! 2
+atIndex :: Int -> [a] -> a
+atIndex n myList | n == 0    = head myList
+                 | otherwise = last (take (n+1) myList)
 
 -- b) generate a list of tuples (n,s)
 tupleList = [ (x,y) | x <- [3,6..3*25], y <- [sum [1..x]]]
 
 -- c) remove all instances from a list
+
