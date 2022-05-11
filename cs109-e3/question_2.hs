57a0a9d7ee1fc18cb5fe8049143815dac37f6f8d
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-11 19:48:15 +0200

    exercise 2 works, not elegant...

--- cs109-e3/question_2.hs
+++ cs109-e3/question_2.hs
@@ -5,5 +5,8 @@ odds = [1,3..]
 recTuples = zip odds (map (1/) odds)
 
 -- c) take list of tuples and a double
-checkSum = 0
-sumTuples tupleLst d = takeWhile (/= (1,1)) tupleLst
+sumTuples tupleLst d = helper tupleLst d 0 0
+
+helper tupleList d idx mySum
+    | mySum >= d    = []
+    | otherwise     = tupleList !! idx : helper tupleList d (idx + 1) (mySum + snd (tupleList !! idx))
