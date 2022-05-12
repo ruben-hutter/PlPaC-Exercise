54d793fe1aefd7329e3b29bdf9df57d6d1ff8415
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-12 20:08:40 +0200

    optimized 3

--- cs109-e3/question_3.hs
+++ cs109-e3/question_3.hs
@@ -6,8 +6,8 @@ ackFun m n p
     | otherwise = ackFun m (ackFun m (n-1) p) (p-1)
 
 ackermannList:: [Int] -> [Int]
-ackermannList [] = [12]
-ackermannList [x] = [(ackFun x 0 3)]
-ackermannList [x, y] = [(ackFun x y 0)]
-ackermannList [x, y, z] = [(ackFun x y z)]
-ackermannList (x:y:z:rest) = (ackFun x y z):(ackermannList rest)
+ackermannList [] = [13]
+ackermannList [x] = [ackFun x 0 3]
+ackermannList [x, y] = [ackFun x y 0]
+ackermannList [x, y, z] = [ackFun x y z]
+ackermannList (x:y:z:rest) = ackFun x y z:ackermannList rest
