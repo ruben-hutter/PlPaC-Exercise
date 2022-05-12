5352cfe41f91a580ce5f22c5376b8975b042b9e2
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-12 20:09:32 +0200

    correct number first statement ackemannList

--- cs109-e3/question_3.hs
+++ cs109-e3/question_3.hs
@@ -10,4 +10,4 @@ ackermannList [] = [13]
 ackermannList [x] = [ackFun x 0 3]
 ackermannList [x, y] = [ackFun x y 0]
 ackermannList [x, y, z] = [ackFun x y z]
-ackermannList (x:y:z:rest) = ackFun x y z:ackermannList rest
+ackermannList (x:y:z:rest) = ackFun x y z : ackermannList rest
